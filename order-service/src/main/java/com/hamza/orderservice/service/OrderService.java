91ef3065ddf990dd1ba993b846bdea31e2e4de56
Hamza <srkhamza555@gmail.com>
2024-02-05 20:18:00 +0530

    Distributed Tracer Implemented using Micrometer Tracing

--- order-service/src/main/java/com/hamza/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/hamza/orderservice/service/OrderService.java
@@ -6,7 +6,10 @@ import com.hamza.orderservice.dto.OrderRequest;
 import com.hamza.orderservice.model.Order;
 import com.hamza.orderservice.model.OrderLineItems;
 import com.hamza.orderservice.repository.OrderRepository;
+import io.micrometer.tracing.Span;
+import io.micrometer.tracing.Tracer;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -18,10 +21,12 @@ import java.util.UUID;
 @Service
 @RequiredArgsConstructor
 @Transactional
+@Slf4j
 public class OrderService {
 
     private final OrderRepository orderRepository;
     private final WebClient.Builder webClientBuilder;
+    private final Tracer tracer;
 
     public String placeOrder(OrderRequest orderRequest){
         Order order = new Order();
@@ -37,24 +42,34 @@ public class OrderService {
                 .stream()
                 .map(OrderLineItems::getSkuCode).toList();
 
+        log.info("Calling Inventory Service");
 
-        //Call inventory Service and check whether item is in stock
-        InventoryResponse[] inventoryResponses = webClientBuilder.build().get()
-                .uri("http://inventory-service:8082/api/inventory",
-                        uriBuilder -> uriBuilder.queryParam("skuCode",skuCodes).build())
-                        .retrieve()
-                                .bodyToMono(InventoryResponse[].class)
-                                        .block();
+        Span inventoryServiceLookup = tracer.nextSpan().name("Inventory Service Lookup");
 
-        boolean allProductsInStock = Arrays.stream(inventoryResponses)
-                                        .allMatch(InventoryResponse::isInStock);
+        try(Tracer.SpanInScope spanInScope = tracer.withSpan(inventoryServiceLookup.start())){
 
-        if(allProductsInStock) {
-            orderRepository.save(order);
-            return "Order is placed!!!";
-        }else {
-            throw new IllegalArgumentException("Out of stock. Try later.");
+            //Call inventory Service and check whether item is in stock
+            InventoryResponse[] inventoryResponses = webClientBuilder.build().get()
+                    .uri("http://inventory-service:8082/api/inventory",
+                            uriBuilder -> uriBuilder.queryParam("skuCode",skuCodes).build())
+                    .retrieve()
+                    .bodyToMono(InventoryResponse[].class)
+                    .block();
+
+            boolean allProductsInStock = Arrays.stream(inventoryResponses)
+                    .allMatch(InventoryResponse::isInStock);
+
+            if(allProductsInStock) {
+                orderRepository.save(order);
+                return "Order is placed!!!";
+            }else {
+                throw new IllegalArgumentException("Out of stock. Try later.");
+            }
+        } finally {
+            inventoryServiceLookup.end();
         }
+
+
     }
 
     private OrderLineItems mapToDto(OrderLineItemsDto orderLineItemsDto) {
